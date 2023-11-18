e1b6faed539b405e7d85313e633514f7d8787378
majorkim <5035als@naver.com>
2023-11-18 17:40:57 +0900

    231118

--- web/routes/posts.js
+++ web/routes/posts.js
@@ -10,4 +10,11 @@ router.get("/", function (req, res, next) {
 router.get("/insert", function (req, res, next) {
   res.render("index", { title: "글쓰기", pageName: "posts/insert.ejs" });
 });
+
+/* 읽기 */
+router.get("/read", function (req, res, next) {
+  const id = req.query.id;
+
+  res.render("index", { title: "게시글정보", pageName: "posts/read.ejs" ,id:id});
+});
 module.exports = router;
